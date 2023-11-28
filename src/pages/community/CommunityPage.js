c061f4cb62841e3d9211cc193dbd9fb1e747d44f
leggo47 <113258117+thwjddlqslek@users.noreply.github.com>
2023-11-28 21:14:21 +0900

    #5 feat: Before Community UI 구현
    #5 feat: Before Community UI 구현

--- src/pages/community/CommunityPage.js
+++ src/pages/community/CommunityPage.js
@@ -1,8 +1,47 @@
+import { Header, MainContainer, FirstContainer, GridContainer, LevelBox, Typography, LevelTypography, Description, HorizontalLine, EnterButton, Footer } from '../../styles/CommunityStyle';
+
 const CommunityPage = () => {
   return (
-    <div>
-      티어 별 커뮤니티 페이지
-    </div>
+    <>
+      <Header>HEADER</Header>
+      <MainContainer>
+        <FirstContainer>
+          <Typography>COMMUNITY</Typography>
+          <HorizontalLine></HorizontalLine>
+          <p>본인 레벨 이하에만 입장 가능</p>
+          <GridContainer>
+            <LevelBox>
+              <LevelTypography>BRONZE</LevelTypography>
+              <Description>
+              설명 어쩌구 저쩌구~~ 이 단계는 어떤 회원한테 적합하고 설명을 써놓고~~~어떤 문제들이 있는지~only for bronze 12324890675379845~!#@~@#~!#%#$%
+              </Description>
+              <EnterButton>ENTER</EnterButton>
+            </LevelBox>
+            <LevelBox>
+              <LevelTypography>SILVER</LevelTypography>
+              <Description>
+              설명 어쩌구 저쩌구~~ 이 단계는 어떤 회원한테 적합하고 설명을 써놓고~~~어떤 문제들이 있는지~only for bronze 12324890675379845~!#@~@#~!#%#$%
+              </Description>
+              <EnterButton>ENTER</EnterButton></LevelBox>
+            <LevelBox>
+              <LevelTypography>GOLD</LevelTypography>
+              <Description>
+              설명 어쩌구 저쩌구~~ 이 단계는 어떤 회원한테 적합하고 설명을 써놓고~~~어떤 문제들이 있는지~only for bronze 12324890675379845~!#@~@#~!#%#$%
+              </Description>
+              <EnterButton>ENTER</EnterButton></LevelBox>
+            <LevelBox>
+              <LevelTypography>PLATINUM</LevelTypography>
+              <Description>
+              설명 어쩌구 저쩌구~~ 이 단계는 어떤 회원한테 적합하고 설명을 써놓고~~~어떤 문제들이 있는지~only for bronze 12324890675379845~!#@~@#~!#%#$%
+              </Description>
+              <EnterButton>LOCKED</EnterButton></LevelBox>
+          </GridContainer>
+        </FirstContainer>
+      </MainContainer>
+      <Footer>
+        <p>FOOTER</p>
+      </Footer>
+    </>
   )
 }
 

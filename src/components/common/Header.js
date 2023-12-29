1cd34fdc0b9c16edff7061323589bf2e80afff2b
young0519 <wkdwlsdud0519@gmail.com>
2023-12-30 00:16:51 +0900

    #24 feat : 로그인 API 호출

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,6 +1,8 @@
 import * as h from "../../styles/headerStyle"
 import mainLogo from '../../assets/imgs/100shot_icon.png'
 import { useLocation, useNavigate } from "react-router-dom"
+import { useEffect } from "react";
+import axios from "axios";
 
 const Header = ({click}) => {
   const navigate = useNavigate();
@@ -19,6 +21,15 @@ const Header = ({click}) => {
     navigate('/community');
   }
 
+  const gitHubUrl = 'http://54.180.163.188:8080/oauth/authorize/github';
+  const onClickGitlogin = () => {
+    axios({
+      method: 'GET',
+      url: 'http://54.180.163.188:8080/oauth/authorize/github'
+    }).then(response => console.log(response))
+    .then(window.location.href = "http://54.180.163.188:8080/login/oauth/authorize/github")
+  };
+
   return (
     <h.HeaderWrapper>
       <h.LogoWrapper className="logo">
@@ -28,7 +39,7 @@ const Header = ({click}) => {
       <h.MenuWrapper className="menu">
         <button 
           onClick={moveToSolution} 
-          className={location.pathname === "/search-solution" ? "active" : ""}
+          className={location.pathname === "/search-solution" || location.pathname === "/result-solution" ? "active" : "" }
         >SOLUTION</button>
         <button 
           onClick={moveToRecommend} 
@@ -42,7 +53,7 @@ const Header = ({click}) => {
           onClick={moveToCommunity} 
           className={location.pathname === "/community" ? "active" : ""}
         >COMMUNITY</button>
-        <button className="login">LOGIN</button>
+        <button className="login" onClick={onClickGitlogin}>LOGIN</button>
       </h.MenuWrapper>
     </h.HeaderWrapper>
   )

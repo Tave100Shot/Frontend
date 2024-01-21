a3317fc32cff65a5f98e0039f4db14833fd5e5d5
toychip <dlawnsgud427@naver.com>
2024-01-21 18:30:16 +0900

    #32 fix: server endpoint change -> api.100shot.net

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import store from './store';
 import axios  from 'axios';
 
-axios.defaults.baseURL = "http://43.200.95.44:8080/";
+axios.defaults.baseURL = "http://api.100shot.net";
 axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/pages/community/WritePage.js
+++ src/pages/community/WritePage.js
@@ -18,7 +18,7 @@ const WritePage = () => {
   const [content, setContent] = useState("");
   const [attachmentFile, setAttachmentFile] = useState(null);
   const [selectedFileContainers, setSelectedFileContainers] = useState([]);
-  const apiUrl = "http://43.200.95.44:8080/api/post";
+  const apiUrl = "http://api.100shot.net/api/post";
 
   const titleChange = (e) => setTitle(e.target.value);
   const contentChange = (e) => setContent(e.target.value);

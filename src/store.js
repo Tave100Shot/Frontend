03ae3890bd3afb17c42cad4100c3c4bfe4746814
young0519 <kiny0606@naver.com>
2024-03-18 12:04:17 +0900

    #80 Refactor : Reducer.js 파일 위치 변경

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { applyMiddleware, createStore } from "redux";
 import logger from "redux-logger";
-import { Reducer } from "./redux/reducers/reducer";
+import { Reducer } from "./redux/reducer";
 
 export const store = createStore(Reducer, applyMiddleware(logger));
 export default store;

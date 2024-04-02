41ff14b294b3e5ca5394d208009ce90236df5ff5
young0519 <kiny0606@naver.com>
2024-04-02 19:38:29 +0900

    #91 Feat : Main Page 내 버튼에 그림자 효과 추가

--- src/styles/main/mainButtonStyle.js
+++ src/styles/main/mainButtonStyle.js
@@ -8,13 +8,13 @@ export const MainButton = styled.button`
   width : 28rem;
   height : 5.5rem;
   position : relative;
-  border : none;
+  border : 0.1rem solid ${props => props.theme.colors.colorAccent};
   border-radius : 1.5rem;
+  box-shadow: inset -0.4rem -0.4rem 0.7rem 0.1rem ${props => props.theme.colors.colorWhiteShadow};
   
-  font-size : 2rem;
+  font-size : 1.8rem;
   font-family : 'Poppins_Regular';
   background-color :${props => props.theme.colors.colorAccent};
-  box-shadow : 0.5rem 0.56rem 1rem ${props => props.theme.colors.colorShadow}
   letter-spacing : .2rem;
   img {
     width : 2.5rem;
@@ -26,6 +26,10 @@ export const MainButton = styled.button`
   .hoverAfter {
     display :none;
   }
+
+  &:active { 
+    border : none;
+    box-shadow:  inset 0.2rem 0.2rem 0.7rem 0.1rem ${props => props.theme.colors.colorShadow};
 
   &.bojUpdateBtn:hover {
     background-color : ${props => props.theme.colors.colorAccentReverse};

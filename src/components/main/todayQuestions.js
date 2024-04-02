bd1dd8438981f7b0fab9dbb6beaa0f43357f9791
leggo47 <143777085+sungshincrypto@users.noreply.github.com>
2024-04-03 03:37:57 +0900

    #90 feat: 뉴스레터 첫페이지

--- src/components/main/todayQuestions.js
+++ src/components/main/todayQuestions.js
@@ -1,11 +1,20 @@
 import * as t from "../../styles/main/todayQuestionStyle";
+import newsImage from '../../assets/imgs/today.png';
 
 const TodayQuestion = () => {
 
 
   return (
     <t.TodayQuestionsWrapper>
-        오늘의 추천 문제
+      <t.mainBox>
+        <t.informationWrapper>
+        <t.titleBox>백발백준 뉴스레터,<br />
+        <t.Highlight>기술</t.Highlight>과 <t.Highlight>취업</t.Highlight> 정보를 한눈에!</t.titleBox><br />
+        <t.informationBox>요즘 <t.Highlight>HOT</t.Highlight>한 기술들의 대한 정보 뿐만 아니라,<br />내가 모르는 취업 공고까지 받아보세요!</t.informationBox><br />
+        <t.moreButton>더 알아보기</t.moreButton>
+      </t.informationWrapper>
+      <t.Image src={newsImage}/>
+      </t.mainBox>
     </t.TodayQuestionsWrapper>
   )
 }
--- src/styles/main/todayQuestionStyle.js
+++ src/styles/main/todayQuestionStyle.js
@@ -4,6 +4,66 @@ export const TodayQuestionsWrapper = styled.div`
   display: flex;
   justify-content: center;
   height: 40rem;
-  margin : 5rem 0 3rem 0;s
+  margin : 5rem 0 3rem 0;
+  //padding : 0 10rem 0 10rem;
+  //background-color: lightblue;
+`
 
+export const mainBox = styled.div`
+  display : flex;
+  width : 90vw;
+  height : 55vh;
+  justify-content: space-between;
+  background-color: white;
+  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.2);
+  border-radius: 20px;
+`
+
+export const titleBox = styled.div`
+  font-size: 4rem;
+  font-weight: bold;
+`
+export const informationBox = styled.div`
+font-size: 2rem;
+font-weight: 500;
+
+`
+export const moreButton = styled.div`
+display: flex;
+font-size: 2rem;
+font-weight: 600;
+color: white;
+width : 16rem;
+height : 5rem;
+background-color: #0075FF;
+align-items: center;
+justify-content: center;
+text-align: center;
+box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.2);
+border-radius: 10px;
+cursor: pointer;
+&:active {
+  transform: translateY(2px);
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.3);
+}
+
+`
+
+export const informationWrapper = styled.div`
+//display: flex;
+width : flex;
+height : 40vh;
+//background-color: lightblue;
+margin : 5rem 0 5rem 5rem;
+
+`
+export const Highlight = styled.span`
+color: #0075FF;
+`
+
+export const Image = styled.img`
+  width: 20%;
+  height : 70%;
+  //background-color: lightblue;
+  margin: 5rem 4rem 5rem 0;
 `

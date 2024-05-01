c01e76d2efd1989ee4f8a6b3907494581bbf16a8
young0519 <kiny0606@naver.com>
2024-05-02 00:06:34 +0900

    #96 Feat : 관리자 인증 버튼 UI 구현

--- src/pages/manage/manageMainPage.js
+++ src/pages/manage/manageMainPage.js
@@ -1,11 +1,16 @@
 import HeaderManage from "../../components/manage/headerManage";
+import * as m from "../../styles/manage/manageMainStyle"
 
 const ManageMain = () => {
 
     return (
-        <div>
+        <m.ManageContainer>
             <HeaderManage/>
-        </div>
+            <m.ManageValidContainer>
+                <h1>백발백준<br/>관리자 페이지입니다</h1>
+                <button>확인했습니다</button>
+            </m.ManageValidContainer>
+        </m.ManageContainer>
     )
 }
 export default ManageMain;
---
+++ src/styles/manage/manageMainStyle.js
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const ManageContainer = styled.div`
+  display : flex;
+  flex-direction : column;
+  justift-contents : center;
+  align-items : center;
+`;
+export const ManageValidContainer = styled.div`
+  margin : 6rem 0 0 0;
+  box-shadow: 0px 5px 10px ${props => props.theme.colors.colorShadow};
+  border-radius: 20px;
+  width : 90vw;
+  height : 40rem;
+  padding : 3rem;
+  display : flex;
+  flex-direction : column;
+  justify-content: center;
+  align-items : center;
+  h1 {
+    font-size : 3.5rem;
+    text-align: center;
+  }
+  button {
+    margin : 5rem 0 0 0;
+    width : 30rem;
+    height : 5rem;
+    color : #ffffff;
+    background-color : #91d1fa;
+    border : none;
+    border-radius : 1rem;
+    font-size : 2rem;
+    letter-spacing : 0.3rem;
+    cursor : pointer;
+  }
+`;
